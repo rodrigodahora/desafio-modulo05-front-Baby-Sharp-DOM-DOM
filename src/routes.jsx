6a36a958802246eb34b806eea7f58cefdaefff44
jneyneto <neyneto@live.com>
2023-09-12 17:12:58 -0300

    Estrutura inicial

---
+++ src/routes.jsx
@@ -0,0 +1,43 @@
+import {
+    BrowserRouter,
+    Navigate,
+    Outlet,
+    Route,
+    Routes
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import SingIn from "./pages/SingIn";
+
+const token = "true";
+
+function ProtectedRoutes({ redirectTo }) {
+    // const token = localStorage.getItem("token");
+
+    return token ? <Outlet /> : <Navigate to={redirectTo} />
+}
+
+function NotProtectedRoutes({ redirectTo }) {
+    // const token = localStorage.getItem("token");
+
+    return token ? <Navigate to="/Home" /> : <Outlet />
+}
+
+
+export default function MyRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<NotProtectedRoutes />}>
+
+                </Route>
+                <Route element={<ProtectedRoutes />}>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/Login" element={<Login />} />
+                    <Route path="/SingIn" element={<SingIn />} />
+                    <Route path="/Home" element={<Home />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
