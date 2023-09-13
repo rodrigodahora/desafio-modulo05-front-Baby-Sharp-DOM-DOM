544032b1dc29aec7e1a84d172a50e68712f5b160
Adrian Ganzo <adrianganzo@yahoo.com.br>
2023-09-12 21:45:47 -0300

    Login atualizado com o uso context

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,15 @@
+import { useContext } from 'react';
+import { MyContext } from '../../contexts/MyContext';
 import './style.css';
 
 function Login() {
+  const { data, setData } = useContext(MyContext);
+  setData({ ...data, name: "Adrian" });
+
   return (
-    <></>
+    <div>
+      <h1>{data.name}</h1>
+    </div>
   );
 }
 
