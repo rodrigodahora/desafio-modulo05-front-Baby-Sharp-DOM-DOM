ddc180c0f2d3c6c6f427326f44658854ae1b621e
Adrian Ganzo <adrianganzo@yahoo.com.br>
2023-09-13 15:50:36 -0300

    index e Css Iniciado

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -7,8 +7,26 @@ function Login() {
   setData({ ...data, name: "Adrian" });
 
   return (
-    <div>
-      <h1>{data.name}</h1>
+    <div className='container-login'>
+      <div className='bgr-image'>
+        <h1>Gerencie todos os pagamentos da sua empresa em um só lugar.</h1>
+      </div>
+      <div className='container-input'>
+        <h1>Faça seu login!</h1>
+
+        <div className='input-email'>
+          <label htmlFor="email">E-mail</label>
+          <input type="email" placeholder='Digite seu e-mail' />
+        </div>
+
+        <div className='input-password'>
+          <label htmlFor="password">Senha</label>
+          <input type="password" placeholder='Digite sua senha' />
+        </div>
+
+        <button>Entrar</button>
+        <span>Ainda não possui uma conta? Cadastre-se</span>
+      </div>
     </div>
   );
 }
