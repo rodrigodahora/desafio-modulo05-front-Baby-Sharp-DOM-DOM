1403eca6e3e4ce9bebc2c6ff9ec3b9397801f1c9
Adrian Ganzo <adrianganzo@yahoo.com.br>
2023-09-14 19:01:09 -0300

    axios corrigida para api nova, adicionado icones lupa e filtro, corrigido context MyContext

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://api-babysharpdomdom.cyclic.cloud",
     timeout: 10000,
     headers: { "Content-Type": "application/json" }
 })
