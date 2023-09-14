06eab4a9351fa3aa5b24c279c779cc8260c126f8
Adrian Ganzo <adrianganzo@yahoo.com.br>
2023-09-14 16:18:21 -0300

    atualização index e login mais edição axios no package.json

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 import { MyContext } from '../../contexts/MyContext';
+import api from "../../services/api";
 import '../../index.css';
 import './style.css';
 
 function Login() {
+  const navigate = useNavigate();
+
   const { data, setData } = useContext(MyContext);
-  setData({ ...data, name: "Adrian" });
+
+  const [mainError, setMainError] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+
+  function handleChange(e) {
+    const key = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [key]: value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await api.post("/login", data.email);
+      navigate("/Home");
+    } catch (error) {
+      return console.log(error);
+    }
+  }
 
   return (
     <div className='container-login'>
@@ -19,18 +42,34 @@ function Login() {
 
         <div className='input-box'>
           <label htmlFor="email">E-mail</label>
-          <input type="email" placeholder='Digite seu e-mail' />
-          <span>Este campo deve ser preenchido</span>
+          <input
+            type="email"
+            name='email'
+            value={data.email}
+            placeholder='Digite seu e-mail'
+            onChange={handleChange}
+          />
+          <span>{errorEmail}</span>
         </div>
 
         <div className='input-box'>
-          <label htmlFor="password">Senha</label>
-          <input type="password" placeholder='Digite sua senha' />
-          <span></span>
+          <label htmlFor="senha">Senha</label>
+          <input
+            type="password"
+            name='senha'
+            placeholder='Digite sua senha'
+            onChange={handleChange}
+          />
+          <span>{errorPassword}</span>
         </div>
 
-        <button>Entrar</button>
-        <p>Ainda não possui uma conta? <a href="/SingIn">Cadastre-se</a></p>
+        <button
+          type='submit'
+          onClick={handleSubmit}
+        >
+          Entrar
+        </button>
+        <p>Ainda não possui uma conta? <a onClick={() => { navigate("/SingIn") }}>Cadastre-se</a></p>
       </div>
     </div>
   );
