c3271064004b8a9f875429df63f3531bdbd030d8
RCaldeira01 <rafael.pdscaldeira@gmail.com>
2023-09-13 22:37:20 -0300

    Home quase finalizada

---
+++ src/components/ListTabs/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const ListTabs = () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.charges}>
+        <div className={styles.paid_charges}>
+          <div className={styles.paid_img}></div>
+          <div className={styles.paid_value}>
+            <h2>Cobranças Pagas</h2>
+            <strong>R$ 30.000</strong>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.won}>
+        <div className={styles.won_charges}>
+          <div className={styles.won_img}></div>
+          <div className={styles.won_value}>
+            <h2>Cobranças Vencidas</h2>
+            <strong>R$ 7.000</strong>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.expected}>
+        <div className={styles.expected_charges}>
+          <div className={styles.expected_img}></div>
+          <div className={styles.expected_value}>
+            <h2>Cobranças Previstas</h2>
+            <strong>R$ 10.000</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListTabs;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import MenuSidebar from '../../components/MenuSidebar';
 import HeaderDash from '../../components/HeaderDash';
+import '../../index.css';
 
 import styles from './styles.module.css';
+import ListTabs from '../../components/ListTabs';
+import ChargesTabs from '../../components/ChargesTabs';
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <MenuSidebar />
 
-      <div>
+      <div className={styles.containerChild}>
         <HeaderDash />
+
+        <ListTabs />
+
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            padding: '10px 50px',
+          }}
+        >
+          <ChargesTabs />
+          <ChargesTabs />
+        </div>
       </div>
     </div>
   );
