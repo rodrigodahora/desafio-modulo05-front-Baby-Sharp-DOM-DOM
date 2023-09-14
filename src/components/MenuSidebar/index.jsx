1a2e48f787ce23b30d28863eacdc8891bbf898a0
RCaldeira01 <rafael.pdscaldeira@gmail.com>
2023-09-14 18:50:55 -0300

    Home quase finalizada

--- src/components/MenuSidebar/index.jsx
+++ src/components/MenuSidebar/index.jsx
@@ -1,21 +1,71 @@
 import React from 'react';
 import styles from './styles.module.css';
 import '../../index.css';
+import home from '../../assets/home.svg';
+import homeSelect from '../../assets/home_select.svg';
+import client from '../../assets/cliente_menu.svg';
+import clientSelect from '../../assets/clientes_selected.svg';
+import charge from '../../assets/cobranca_menu.svg';
+
+import { useContext } from 'react';
+import { MyContext } from '../../contexts/MyContext';
 
 const MenuSidebar = () => {
+  const { selected } = useContext(MyContext);
+
   return (
     <ul className={styles.container}>
-      <li className={styles.home}>
-        <div></div>
-        <a href="/home">Home</a>
+      <li
+        className={
+          selected === 1 ? `${styles.home}  ${styles.selected}` : styles.home
+        }
+      >
+        <div>
+          <img src={selected === 1 ? homeSelect : home} alt="Button-home" />
+        </div>
+        <a
+          className={selected === 1 ? styles.link : styles.no_link}
+          href="/home"
+        >
+          Home
+        </a>
       </li>
-      <li className={styles.clients}>
-        <div></div>
-        <a href="/clients">Clientes</a>
+      <li
+        className={
+          selected === 2
+            ? `${styles.clients}  ${styles.selected}`
+            : styles.clients
+        }
+      >
+        <div>
+          <img
+            src={selected === 2 ? clientSelect : client}
+            alt="Button-client"
+          />
+        </div>
+        <a
+          className={selected === 2 ? styles.link : styles.no_link}
+          href="/clients"
+        >
+          Clientes
+        </a>
       </li>
-      <li className={styles.charges}>
-        <div></div>
-        <a href="/charges">Cobranças</a>
+      <li
+        className={
+          selected === 3
+            ? `${styles.charges}  ${styles.selected}`
+            : styles.charges
+        }
+      >
+        <div>
+          <img src={selected === 3 ? charge : charge} alt="Button-charge" />
+        </div>
+        <a
+          className={selected === 3 ? styles.link : styles.no_link}
+          href="/charges"
+        >
+          Cobranças
+        </a>
       </li>
     </ul>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,19 +6,28 @@ import '../../index.css';
 import styles from './styles.module.css';
 import ListTabs from '../../components/ListTabs';
 import ChargesTabs from '../../components/ChargesTabs';
+import ClientsTabs from '../../components/ClientsTabs';
+
+import { useContext } from 'react';
+import { MyContext } from '../../contexts/MyContext';
 
 const Home = () => {
+  const { setSelected } = useContext(MyContext);
+  setSelected(1);
   return (
     <div className={styles.container}>
+      <HeaderDash />
       <MenuSidebar />
 
       <div className={styles.containerChild}>
-        <HeaderDash />
-
         <ListTabs />
 
         <div>
           <ChargesTabs />
+        </div>
+
+        <div>
+          <ClientsTabs />
         </div>
       </div>
     </div>
