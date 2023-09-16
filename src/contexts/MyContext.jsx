6c7b07bdd22539f298acf3f9a11ba25f3c1a8b0b
Ney <neyneto@live.com>
2023-09-15 22:01:23 -0300

    soft att

--- src/contexts/MyContext.jsx
+++ src/contexts/MyContext.jsx
@@ -5,7 +5,7 @@ export const MyContext = createContext();
 export function ContextProvider(props) {
     const [data, setData] = useState({ name: "", email: "", password: "", confPassword: "" });
     const [selected, setSelected] = useState(1);
-    const [addClient, setAddClient] = useState(true)
+    const [addClient, setAddClient] = useState(false)
 
     return (
         <MyContext.Provider value={{ data, setData, selected, setSelected, addClient, setAddClient }}>
