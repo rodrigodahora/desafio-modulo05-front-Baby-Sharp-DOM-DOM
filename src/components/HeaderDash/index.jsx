1a08b8febeac210eb76624f2e42770de9cf5ef29
Ney <neyneto@live.com>
2023-09-17 11:04:57 -0300

    ajustes header

--- src/components/HeaderDash/index.jsx
+++ src/components/HeaderDash/index.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from './styles.module.css';
 import '../../index.css';
 import edit from '../../assets/editar.svg';
 import logout from '../../assets/logout.svg';
 import arrowTop from '../../assets/arrow_top.svg';
+import { MyContext } from '../../contexts/MyContext';
+
 
 const HeaderDash = ({ setOpenModalUser }) => {
+  const navigate = useNavigate();
+  const { selected, setSelected } = useContext(MyContext);
   const [modalUser, setModalUser] = useState(false);
 
   return (
     <div className={styles.header_dash}>
       <div className={styles.container}>
-        <h1>Resumo de cobranças</h1>
+        {selected === 1 && <h1>Resumo de cobranças</h1>}
+        {selected === 2 && <h2>Clientes</h2>}
+        {selected === 3 && <h1>Resumo de cobranças</h1>}
 
         <div className={styles.login}>
           <div className={styles.profile}>LR</div>
